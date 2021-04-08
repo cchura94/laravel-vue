43dd75b12504bd09c4bc94c9e00a238811e51637
cchura94 <cchura.cpc@gmail.com>
2021-04-08 18:42:55 -0400

    primer cambio

---
+++ resources/js/components/TarjetaComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  
+  <div class="card" style="width: 18rem;">
+    <img src="https://www.kibernum.com/wp-content/uploads/2019/12/nuevas-tecnologias.png" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+   .card{
+       float: left;
+   }
+</style>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="/css/app.css">
+
+
+<h1>Renderizando Componentes</h1>
+<div id="app">
+    <cristian></cristian>
+    <hr>
+    <hola></hola>
+    <hola></hola>
+    <hola></hola>
+    <hola></hola>
+    <hola></hola>
+
+</div>
+
+<script src="/js/app.js"></script>
